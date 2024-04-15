23e129d8327c65933ad8fa9e5aba4e2c1a02dc88
정경재 <arad4228@gmail.com>
2024-04-15 20:17:40 +0900

    [KyoungJae]Add NodeV BroadCast Code

--- SmartCity/Node.py
+++ SmartCity/Node.py
@@ -1,6 +1,5 @@
 from socket import *
 import threading
-import time
 import hashlib
 import av
 
@@ -10,47 +9,67 @@ class NodeV:
     __strNodeName           :str
     __strNodeRole           :str
     __pairKeys              :list
-    __listNodes             :list
+    __listNodes             :dict
     __nInitialNodeNumber    :int
-    __dictReceivedData      :dict  # key: strNode, value: strIP
+    __dictReceivedData      :dict  # key: strNodeRole, value: strIP
     __lockThread            :threading.Lock
     __listThread            :list
     __listFrames            :dict
+
+    __socketSendFrame       :socket
+    __socketReceived        :socket
     
-    def __init__(self, strName  :str, strRole='Validator'):
+    def __init__(self, strName  :str, port):
         self.__strNodeName = strName
-        self.__strNodeRole = strRole
+        self.__strNodeRole = 'Validator'
         self.__lockThread = threading.Lock()
-    
+        # 받고 보낼 소켓 생성
+        self.__socketSendFrame = socket(AF_INET, SOCK_DGRAM)
+        self.__socketReceived = socket(AF_INET, SOCK_DGRAM)
+        # 보내는 소켓은 BroadCast, 받는 소캣은 전부 수용하도록 설정
+        self.__socketSendFrame.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.__socketReceived.bind(('', port))
+
     def setInitialNodeNumber(self, nNodeNum: int):
         self.__nInitialNodeNumber = nNodeNum
 
     def getOwnSecrete(self):
         return self.__pairkeys
     
-    def broadCastNodeData(self):
-        # Lock을 얻고 몇개의 데이터를 가지고 있는지 리스트를 확인
-        self.__lockThread.aquire()
-        if len(self.__listNodes) >= self.__nInitialNodeNumber:
+    def broadCastNodeData(self, port):
+        while True:
+            # Lock을 얻고 몇개의 데이터를 가지고 있는지 리스트를 확인
+            self.__lockThread.aquire()
+            if len(self.__listNodes) >= self.__nInitialNodeNumber:
+                self.__lockThread.release()
+                break
             self.__lockThread.release()
-            return True
-        
-        self.__lockThread.release()
-        ## 모든 네트워크 대역에 Broadcast
+
+            ## 모든 네트워크 대역에 Broadcast
+            message = (self.__strNodeName + self.__strNodeRole).encode("UTF-8")
+            self.__socketSendFrame.sendto(message, ('<broadcast>', port))
 
     def receivedNodeData(self):
-        ## 패킷을 전달받고.
-        
-        ## 전달받는 로직(ToBe)
+        while True:
+            self.__lockThread.aquire()
+            if len(self.__listNodes) >= self.__nInitialNodeNumber:
+                self.__lockThread.release()
+                break
+            self.__lockThread.release()
 
-        newNode = list()
-        self.__lockThread.acquire()
-        self.__listNodes.append(newNode)
-        self.__lockThread.release()
+            len = self.__socketReceived.recv(1)
+            # nodeData = NodeName||NodeRole
+            nodeData, addr = self.__socketReceived(len)
+            splitData = nodeData.split(',')
+
+            self.__lockThread.acquire()
+            self.__listNodes[addr] = splitData
+            self.__lockThread.release()
 
     def sendListNodeData(self):
         # BroadCast Node List data;
-        # length of Node | list data
+        # length of Node | key: Ip value: NodeName||NodeRole
+        message = str(len(self.__listNodes))
         print('temp')
 
     def receivedSensorData(self, strNodeName):
@@ -75,7 +94,7 @@ class NodeV:
 
 class NodeSV(NodeV):
     __strSeosorURL          :str
-    __listSensorFrame             :list
+    __listSensorFrame       :list
 
     def __init__(self, strName  :str, strURL    :str):
         super.__init__(strName, "Sensor")
