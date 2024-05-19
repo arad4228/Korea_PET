bff55030b6593e4beaf049851bd9565319d7425f
arad4228 <arad4228@gmail.com>
2024-05-19 15:28:53 +0900

    [KyoungJae]Fix NetworkSetup Script

--- SmartCity/deleteNetwork.sh
+++ SmartCity/deleteNetwork.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
 
-# 필요한 네임스페이스 수
-NUM_NAMESPACES=$1
+# 필요한 인터페이스 수
+NUM_INTERFACES=$1
 
-if [ -z "$NUM_NAMESPACES" ]; then
-  echo "Usage: $0 <number-of-namespaces>"
+if [ -z "$NUM_INTERFACES" ]; then
+  echo "Usage: $0 <number-of-interfaces>"
   exit 1
 fi
 
-# 네임스페이스 및 veth 인터페이스 제거
-for i in $(seq 1 $NUM_NAMESPACES); do
-  NS="ns$i"
-  VETH_HOST="veth_host$i"
+BRIDGE_NAME=br0
 
-  # 네임스페이스 제거
-  ip netns del $NS
-  if [ $? -ne 0 ]; then
-    echo "Failed to delete namespace $NS"
-  fi
-
-  # 호스트 측 veth 인터페이스 제거
-  ip link del $VETH_HOST
-  if [ $? -ne 0 ]; then
-    echo "Failed to delete veth interface $VETH_HOST"
-  fi
+# 가상 이더넷 인터페이스 제거
+for i in $(seq 0 $(($NUM_INTERFACES-1)))
+do
+    ip link del veth$i
 done
 
 # 브리지 제거
-BRIDGE_NAME="br0"
-ip link set $BRIDGE_NAME down
-ip link del $BRIDGE_NAME
-if [ $? -ne 0 ]; then
-  echo "Failed to delete bridge $BRIDGE_NAME"
-fi
+ip link del $BRIDGE_NAME 2>/dev/null
 
-echo "Virtual network cleanup complete."
+echo "가상 네트워크 설정이 제거되었습니다."
--- SmartCity/setupNetwork.sh
+++ SmartCity/setupNetwork.sh
@@ -1,80 +1,41 @@
 #!/bin/bash
 
-# 필요한 네임스페이스 수
-NUM_NAMESPACES=$1
+# 필요한 인터페이스 수
+NUM_INTERFACES=$1
 
-if [ -z "$NUM_NAMESPACES" ]; then
-  echo "Usage: $0 <number-of-namespaces>"
+if [ -z "$NUM_INTERFACES" ]; then
+  echo "Usage: $0 <number-of-interfaces>"
   exit 1
 fi
+
+BRIDGE_NAME=br0
 
 # 브리지 생성
-BRIDGE_NAME="br0"
 ip link add name $BRIDGE_NAME type bridge
-if [ $? -ne 0 ]; then
-  echo "Failed to create bridge $BRIDGE_NAME"
-  exit 1
-fi
-ip addr add 10.10.10.1/24 dev $BRIDGE_NAME
 ip link set $BRIDGE_NAME up
-if [ $? -ne 0 ]; then
-  echo "Failed to set bridge $BRIDGE_NAME up"
-  exit 1
-fi
 
-# IP 포워딩 및 브로드캐스트 설정
-sysctl -w net.ipv4.ip_forward=1
-echo 0 > /proc/sys/net/bridge/bridge-nf-call-iptables
-echo 0 > /proc/sys/net/bridge/bridge-nf-call-ip6tables
-
-# 네임스페이스 생성 및 설정
-for i in $(seq 1 $NUM_NAMESPACES); do
-  NS="ns$i"
-  VETH_HOST="veth_host$i"
-  VETH_NS="veth_ns$i"
-  IP_NS="10.10.10.$((i+1))"
-
-  # 네임스페이스 생성
-  ip netns add $NS
-  if [ $? -ne 0 ]; then
-    echo "Failed to create namespace $NS"
-    continue
-  fi
-
-  # veth 쌍 생성
-  ip link add $VETH_HOST type veth peer name $VETH_NS
-  if [ $? -ne 0 ]; then
-    echo "Failed to create veth pair $VETH_HOST and $VETH_NS"
-    ip netns del $NS
-    continue
-  fi
-
-  # veth 인터페이스를 네임스페이스에 연결
-  ip link set $VETH_NS netns $NS
-  if [ $? -ne 0 ]; then
-    echo "Failed to set $VETH_NS to namespace $NS"
-    ip link del $VETH_HOST
-    ip netns del $NS
-    continue
-  fi
-
-  # 네임스페이스 내부 인터페이스 설정
-  ip netns exec $NS ip addr add $IP_NS/24 dev $VETH_NS
-  ip netns exec $NS ip link set $VETH_NS up
-  ip netns exec $NS ip route add default via 10.10.10.1
-
-  # 호스트 측 인터페이스 설정
-  ip addr add 10.10.10.$((100 + i))/24 dev $VETH_HOST
-  ip link set $VETH_HOST up
-
-  # 브리지에 인터페이스 연결
-  ip link set $VETH_HOST master $BRIDGE_NAME
-
-  # 네임스페이스 내에서 브로드캐스트 주소 설정
-  ip netns exec $NS ip route add broadcast 10.10.10.255 dev $VETH_NS
+# 가상 이더넷 인터페이스 생성 및 브리지에 연결
+for i in $(seq 0 $(($NUM_INTERFACES-1)))
+do
+    # 단일 veth 인터페이스 생성
+    ip tuntap add dev veth$i mode tap
+    
+    # IP 할당 및 활성화
+    ip addr add 10.0.0.$(($i+1))/24 broadcast 10.255.255.255 dev veth$i
+    ip link set veth$i up
+    
+    # veth 인터페이스를 브리지에 연결
+    ip link set veth$i master $BRIDGE_NAME
 done
 
-# 브로드캐스트 및 P2P 통신을 위한 iptables 설정 (필요 시 추가)
-iptables -A FORWARD -i $BRIDGE_NAME -o $BRIDGE_NAME -j ACCEPT
+# 브리지에 IP 할당
+ip addr add 10.0.0.254/24 broadcast 10.255.255.255 dev $BRIDGE_NAME
 
-echo "Virtual network setup complete."
+# 상태 확인
+ip addr show $BRIDGE_NAME
+for i in $(seq 0 $(($NUM_INTERFACES-1)))
+do
+    ip addr show veth$i
+done
+
+echo "가상 네트워크 설정 완료. 생성된 인터페이스끼리 통신 및 브로드캐스트가 가능합니다."
