c1b85cd6303f78ce2e52b5a53cdeffe441101d14
arad4228 <arad4228@gmail.com>
2024-05-24 21:50:59 +0900

    [KyoungJae]Remove Unused Package & Add Verify Func(not stable)

--- SmartCity/Node.py
+++ SmartCity/Node.py
@@ -14,11 +14,7 @@ import cv2
 import requests
 import pprint
 import os
-import numpy as np
 import traceback
-from base64 import b64encode
-import base64
-import pickle
 
 
 MAX_Size = 10000
@@ -35,12 +31,12 @@ class NodeV:
     __eventSocket                   :Event
     __listThread                    :list
     __dictReceivedFrames            :dict           # Received Frames "strNodeName" : [Frame, ...]
-    __IV                            :bytearray
-
-    __ownIP                         :str
-    __nPort                         :int
-    __socketReceived                :socket
-    __socketBroadcastSend           :socket
+    
+    IV                              :bytearray
+    ownIP                           :str
+    nPort                           :int
+    socketReceived                  :socket
+    socketBroadcastSend             :socket
 
     def __init__(self, strNodeName, ownIP):
         self.__strNodeName = strNodeName
@@ -50,24 +46,25 @@ class NodeV:
         self.__eventSocket = Event()
         self.__listThread = list()
         self.__dictReceivedFrames = dict()
-        self.__IV   = b'0123456789101112'
-        self.__nPort = 9000
-        self.__ownIP = ownIP
+        self.IV   = b'0123456789101112'
+        self.nPort = 9000
+        self.ownIP = ownIP
         self.__nInitialNodeNumber = 0
-        self.__broadCastIP = ownIP[:3]+"255.255.255"
+        self.broadCastIP = ownIP[:3]+"255.255.255"
+        self.sendFrameBytes = 1024
         
         # 받고 보낼 소켓 생성
-        self.__socketReceived = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.__socketBroadcastSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socketReceived = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socketBroadcastSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # 보내는 소켓은 BroadCast, 받는 소켓은 전부 수용하도록 설정
-        self.__socketReceived.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.__socketReceived.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.__socketReceived.bind(('', self.__nPort))
+        self.socketReceived.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socketReceived.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.socketReceived.bind(('', self.nPort))
         
-        self.__socketBroadcastSend.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.__socketBroadcastSend.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.__socketBroadcastSend.bind((self.__ownIP, 0))
+        self.socketBroadcastSend.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.socketBroadcastSend.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socketBroadcastSend.bind((self.ownIP, 0))
         
     def __image_to_bytes(self, image):
         img_byte_arr = io.BytesIO()
@@ -76,7 +73,7 @@ class NodeV:
         return img_byte_arr
 
     def __broadCastNodeData(self):
-        dict_data = {self.__strNodeName : {"IP": self.__ownIP, "Role": self.__strNodeRole, "PublicKey" : self.__pubKeyNode}}
+        dict_data = {self.__strNodeName : {"IP": self.ownIP, "Role": self.__strNodeRole, "PublicKey" : self.__pubKeyNode}}
         jsonNodeData = json.dumps(dict_data)
     
         if self.__nInitialNodeNumber == 0:
@@ -96,21 +93,21 @@ class NodeV:
             typeSendB = struct.pack('c', b'B')
             message = typeSendB + jsonNodeData.encode('utf-8')
             # 모든 네트워크 대역에 Broadcast
-            self.__socketBroadcastSend.sendto(message, (self.__broadCastIP, self.__nPort))
+            self.socketBroadcastSend.sendto(message, (self.__broadCastIP, self.nPort))
             sleep(3)
 
         # 만약 누군가 끝났다고 Packet을 보내지 않았다면 아래를 수행.
         # 초기 노드들이 수집되었다면, 끝을 알리고, 다음을 진행.
         if not self.__eventSocket.is_set():
             typeSendE = struct.pack('c', b'E')
-            self.__socketBroadcastSend.sendto(typeSendE, (self.__broadCastIP, self.__nPort))
+            self.socketBroadcastSend.sendto(typeSendE, (self.__broadCastIP, self.nPort))
         
         # 자신이 가진 데이터를 모두 전송.
         # N||NodeList
         typeSendN = struct.pack('c', b'N')
         dataEncoded = json.dumps(self.__dictReceivedData).encode('utf-8')
         message = typeSendN + dataEncoded
-        self.__socketBroadcastSend.sendto(message, (self.__broadCastIP, self.__nPort))
+        self.socketBroadcastSend.sendto(message, (self.__broadCastIP, self.nPort))
         
     def __receivedNodeData(self):
         while True:
@@ -121,7 +118,7 @@ class NodeV:
             self.__lockThread.release_lock()
             
            # type, size check
-            initialData, addr = self.__socketReceived.recvfrom(4096)
+            initialData, addr = self.socketReceived.recvfrom(4096)
             message_type = struct.unpack('c', initialData[:1])[0]
             # End
             if message_type == b'E':
@@ -136,7 +133,7 @@ class NodeV:
 
         # E 타입의 메시지를 받았다면, N 타입의 패킷을 수용하여, 노드정보 업데이트.
         while True:
-            nodeListData, addr = self.__socketReceived.recvfrom(10240)
+            nodeListData, addr = self.socketReceived.recvfrom(10240)
             message_type = struct.unpack('c', nodeListData[:1])[0]
             if message_type == b'N':
                 dictNodeData = json.loads(nodeListData[1:].decode('UTF-8'))
@@ -209,41 +206,47 @@ class NodeV:
         # pubKeyNode  = nodeData['PublicKey']
         # if pubKeyNode == None:
         #     raise Exception(f"{strNodeName}의 공개키가 존재하지 않습니다.")
-        testKey = '5aa5e74167a468b756402958f8c7db6ee3f0349d6385e61d4649236a89474af13ce37fdbe847c59636d6506510c91e899c0e35dacd30db10fc0d3ee675b2aa79'
-        pubKeyNode=VerifyingKey.from_string(bytes.fromhex(testKey), curve=NIST256p)
+        
+        # testKey = '8063eeafdccd2dd0c6a121824ab966482be2934b02a6bef0112aaa53e9c85c930ff5c701c7493f0190849dfc8f6f7bf9e4f09f044eaf7418adb8bf4ea94fff2a'
+        # pubKeyNode=VerifyingKey.from_string(bytes.fromhex(testKey), curve=NIST256p)
+        
         listFrameData = list()
         counter = 0
         try:
             while True:
-                typePacket = self.__socketReceived(1)
-                typePacket = struct.unpack('c', typePacket[0])
+                # S||nFrameList||SID,SIG(F||AddrIPFS),AddrIPFS
+                receivedData, addr = self.socketReceived.recvfrom(4096)
+                typePacket = struct.unpack('c', receivedData[:1])[0]
                 if typePacket != b'S':
                     continue
                 
-                # S||nFrameList||nData||SID,SIG(F||AddrIPFS),Frame,AddrIPFS
-                totalFrames = self.__socketReceived.recv(5000) #packet lenth - +200~300
-                print(len(totalFrames))
-                totalFrames = struct.unpack('Q', totalFrames[0])
-                nFrameSize = self.__socketReceived.recv(8)
-                nFrameSize = struct.unpack('Q', nFrameSize[0]) # long long
-                
-                recevedData = self.__socketReceived.recv(nFrameSize)
-                splitedData = recevedData.split(',')
+                totalFrames = struct.unpack('Q', receivedData[1:9])[0]
+                splitingData = receivedData[9:].decode('UTF-8')
+                splitedData = splitingData.split(',')
                 if splitedData[0] != strNodeName:
                     continue
                 if counter == totalFrames:
                     break
                 
                 signature = splitedData[1]
-                frame = splitedData[2]
-                addrIPFS = splitedData[3]
+                addrIPFS = splitedData[2]
+                frame = b''
+                while True:
+                    receivedFrame, addr = self.socketReceived.recvfrom(self.sendFrameBytes)
+                    if receivedFrame == b'E':
+                        break
+                    frame = frame + receivedFrame
+                # with open('reframe.txt', 'w') as f:
+                #     f.write(frame.hex())
+                
                 message = f'{frame}{addrIPFS}'.encode("UTF-8")
-                ret = pubKeyNode.verify(signature, message, sha256)
+                ret = pubKeyNode.verify(bytes.fromhex(signature), message, sha256)
                 if not ret:
                     raise Exception(f"{pubKeyNode}의 {addrIPFS}의 서명 검증에 실패했습니다.")
                 
                 listFrameData.append(frame)
                 counter += 1
+                print(f'현재:{counter}, 전체:{totalFrames}')
                 
             self.__dictReceivedFrames[strNodeName] = listFrameData
             print(f"{strNodeName}의 총 {totalFrames} Frame을 저장하였습니다.")
@@ -294,8 +297,7 @@ class NodeSV(NodeV):
         
         pubkey = self.getOwnPublicKey().to_string()
         token = self.__generateToken(pubkey)
-        print(type(token))
-        key = pubkey & token
+        key = bytes(a ^ b for a, b in zip(pubkey, token))
         cipher = AES.new(key, AES.MODE_CBC, self.__IV)
 
         with open(fileName, 'rb') as f:
@@ -326,26 +328,29 @@ class NodeSV(NodeV):
             
             if not cap.isOpened():
                 raise Exception(f"{self.__strSensorURL} 주소에서 데이터를 받을 수 없습니다.")
+            
             videoFrame = list()
             start_time = cv2.getTickCount() / cv2.getTickFrequency()
             while True:
                 ret, frame = cap.read()
                 if not ret:
                     break
-                frame_byte_b64=cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
-                print(len(frame_byte_b64))
-                frame_byte=bytearray(frame_byte_b64[1]).hex()
-                print(type(frame_byte))
-                print(len(frame_byte))
-                print(frame_byte[:100])
-                videoFrame.append(frame_byte)
+                
+                ret, buffer = cv2.imencode('.jpg', frame)
+                if not ret:
+                    raise Exception("받은 Frame을 image로 변경할 수 없습니다.")
+                
+                byteFrame = buffer.tobytes()
+                videoFrame.append(byteFrame)
                 out.write(frame)
                 
                 current_time = cv2.getTickCount() / cv2.getTickFrequency()
                 if current_time - start_time >= timeDelay:
                     break
+                
             cap.release()
             out.release()
+            
             # IPFS 업로드
             #res = self.__uploadSensorDataIPFS(filePath)
             #print(res)
@@ -359,7 +364,6 @@ class NodeSV(NodeV):
             
         except Exception as e:
             print(f"다음과 같은 예외가 발생했습니다.\n{e}")
-            print(traceback.format_exc())
 
     def sendSensorData(self, timeSensor):
         collectionsSensorData = self.__dicttSensorData[timeSensor]
@@ -371,33 +375,30 @@ class NodeSV(NodeV):
         nListSensorFrame = len(listFrames)
         packedLengthFrames = struct.pack('Q', nListSensorFrame)
         strNodeName = self.getNodeName()
-        nSID = struct.pack('i', len(strNodeName))
         
-        # S||nFrameList||nData||SID,SIG(F||AddrIPFS),Frame,AddrIPFS
-        # S||nFrameList||nSID
+        # S||nFrameList||SID,SIG(F||AddrIPFS),AddrIPFS,Frame
+        # S||nFrameList
         bcastmessage = packedType + packedLengthFrames
         
         priv = self.getOwnPrivateKey()
         for frame in listFrames:
-            print(type(frame))
-            print(type(frame))
-            print(type(addrIPFS))
+            # with open('frame.txt', 'w') as f:
+            #     f.write(frame.hex())
             messageSign = f'{frame}{addrIPFS}'.encode("UTF-8")
             sig = priv.sign_deterministic(
                 messageSign,
                 hashfunc=sha256
             )
             
-            print(len(bcastmessage))
-            print(len(nSID))
-            print(len(strNodeName))
-            print(len(sig))
-            print(len(frame))
-            print(len(addrIPFS))
+            # S||nFrameList||SID,SIG(F||AddrIPFS),AddrIPFS
+            sendMessage = bcastmessage + f'{strNodeName},{sig.hex()},{addrIPFS}'.encode('UTF-8')
+            self.socketBroadcastSend.sendto(sendMessage, (self.broadCastIP, self.nPort))
             
-            SendMessage = bcastmessage.hex() + nSID.hex() + f'{strNodeName},{sig.hex()},{frame},{addrIPFS}'
-            bytesSendMessage=bytes(SendMessage, 'utf-8')
-            #self.__socketBroadcastSend.sendto(sendMessage, (self.__broadCastIP, self.__nPort))
-            print(len(bytesSendMessage))
-            self._NodeV__socketBroadcastSend.sendto(bytesSendMessage, (self._NodeV__broadCastIP, self._NodeV__nPort))
-           
+            if len(frame) % self.sendFrameBytes != 0:
+                sendCount = len(frame) // self.sendFrameBytes + 1
+            else:
+                sendCount = len(frame) // self.sendFrameBytes
+            
+            for i in range(sendCount):
+                self.socketBroadcastSend.sendto(frame[i*1024:(i+1)*1024], (self.broadCastIP, self.nPort))
+            self.socketBroadcastSend.sendto(b"E", (self.broadCastIP, self.nPort))
