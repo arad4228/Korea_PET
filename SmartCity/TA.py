963283a75118d21924c67b815b79ca92e1827bf6
정경재 <arad4228@gmail.com>
2024-05-29 10:54:12 +0900

    [KyoungJae]Rename KGC to TA

--- SmartCity/TA.py
+++ SmartCity/TA.py
@@ -2,18 +2,31 @@ from ecdsa import SigningKey, VerifyingKey, NIST256p
 from collections import OrderedDict
 from web3 import Web3
 from solcx import install_solc, set_solc_version, compile_source
+from pprint import pprint
 import json
 import os
 
-class KGC:
-    __pubkeyKGC             :VerifyingKey
-    __privkeyKGC            :SigningKey
+class TA:
+    __addressEth            :str
+    __pubkeyTA              :VerifyingKey
+    __privkeyTA             :SigningKey
     __web3                  :Web3
     __strsmartContract      :str
     
-    def __init__(self):
-        self.__privkeyKGC = SigningKey.generate(curve=NIST256p)
-        self.__pubkeyKGC = self.__privkeyKGC.verifying_key
+    def __init__(self, addressEth, socketW3,):
+        self.__privkeyTA = SigningKey.generate(curve=NIST256p)
+        self.__pubkeyTA = self.__privkeyTA.verifying_key
+
+        self.__addressEth = addressEth
+        self.__web3 = Web3(Web3.HTTPProvider(f'{addressEth}:{socketW3}'))
+        if not self.__web3.is_connected():
+            print("ETH에 접근할 수 없습니다. 아래와 같은 IP주소와 Port번호를 입력해주세요. \nex)address: http://127.0.0.1 port: 7545")
+            exit(-1)
+            
+        # TA의 계정은 0번
+        self.__web3.eth.default_account = self.__web3.eth.accounts[0]
+        self.__strsmartContract = None
+       
     
     def generatePrivPubkey(self, listNodeName, nClient):
         # "ID" : {"Priv": "Pub"}
@@ -42,14 +55,10 @@ class KGC:
         with open(fileName, 'r') as f:
             self.__strsmartContract = f.read()
                 
-    def deploySmartContact(self, addressEth, socketW3, versionSolc):
+    def deploySmartContact(self, versionSolc):
         try:
-            self.__web3 = Web3(Web3.HTTPProvider(f'{addressEth}:{socketW3}'))
-            if not self.__web3.is_connected():
-                raise Exception("ETH에 접근할 수 없습니다. 아래와 같은 IP주소와 Port번호를 입력해주세요. \nex)address: http://127.0.0.1 port: 7545")
-            
-            # KGC의 계정은 0번
-            self.__web3.eth.default_account = self.__web3.eth.accounts[0]
+            if self.__strsmartContract == None:
+                raise Exception("배포하고자하는 SmartContract에 대해 로딩이 필요합니다.")
             
             version = install_solc(versionSolc)
             print(f'Solc version: {version}')
@@ -73,7 +82,7 @@ class KGC:
                     smartContract = self.__web3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin']) 
                     contractTxHash  = smartContract.constructor().transact()
                     contractTxReceipt = self.__web3.eth.wait_for_transaction_receipt(contractTxHash)
-                    print(f"{addressEth}의 ETH에 다음과 같은 Smart Contract가 배포되었습니다.\n{contract_id}:{contractTxReceipt.contractAddress}")
+                    print(f"{self.__addressEth}의 ETH에 다음과 같은 Smart Contract가 배포되었습니다.\n{contract_id}:{contractTxReceipt.contractAddress}")
                     
                     innerDict = dict()
                     innerDict['abi'] = contract_interface['abi']
@@ -89,3 +98,6 @@ class KGC:
                 
         except Exception as e:
             print(e)
+    
+    def getEthBlockInfo(self):
+        pprint(dict(self.__web3.eth.get_block('latest')), indent=4)
