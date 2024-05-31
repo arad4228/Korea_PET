904ec602f9ae566ddfca1e593e2b09f6b01ae75e
arad4228 <arad4228@gmail.com>
2024-05-31 17:19:49 +0900

    [KyoungJae]Update README, starting script

--- SmartCity/staring.sh
+++ SmartCity/staring.sh
@@ -6,4 +6,5 @@ apt install -y python3 python3-pip tshark nodejs npm
 
 # 필요한 SmartContract자료 다운로드
 npm install --save-dev hardhat
-# npm install @openzeppelin/contracts
+npm install @openzeppelin/contracts
+npm install ganache --global
